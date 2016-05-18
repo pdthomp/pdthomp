b4bb3a213ea21542d2ba20a61568e4ea82e7cac4
Philip Thompson <philip.thompson.dev@gmail.com>
2016-05-18 16:12:37 -0400

    Changed to composer auto loading.

--- module/Album/Module.php
+++ module/Album/Module.php
@@ -2,29 +2,14 @@
 
 namespace Album;
 
-use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 use Zend\Db\ResultSet\ResultSet;
 use Zend\Db\TableGateway\TableGateway;
 use Album\Model\Album;
 use Album\Model\AlbumTable;
 
-class Module implements AutoloaderProviderInterface, ConfigProviderInterface
+class Module implements ConfigProviderInterface
 {
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__ . '/autoload_classmap.php',
-                ),
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                    ),
-                ),
-            );
-    }
-
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
