a6418796dd434e1726bbc57292e5a1c69a5e51c0
Philip Thompson <philip.thompson.dev@gmail.com>
2016-05-23 18:45:39 -0400

    More of blog tut.

--- module/Blog/src/Blog/Controller/ListController.php
+++ module/Blog/src/Blog/Controller/ListController.php
@@ -2,8 +2,14 @@
 
 namespace Blog\Controller;
 
+use Blog\Service\PostServiceInterface;
 use Zend\Mvc\Controller\AbstractActionController;
 
-class ListController extends AbstractActionController
-{
+class ListController extends AbstractActionController {
+	protected $postService;
+
+	public function __construct(PostServiceInterface $postService)
+	{
+		$this->postService = $postService;
+	}
 }
